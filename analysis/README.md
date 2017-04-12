86e239966bd49209d65bf25575f8f3eae13f4bff
Jon Terner <jon.programming@gmail.com>
2017-04-12 13:09:33 -0500

    started feature analysis

--- analysis/README.md
+++ analysis/README.md
@@ -1,1 +1,5 @@
 Scripts that are designed for feature analysis and data cleaning
+
+
+Required Libraries: (can be installed using pip):
+* matplotlib
---
+++ analysis/plotter.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''
+A python script designed to plot any two attributes from our datasets.
+Useful for visualizing correlation
+'''
+
+import matplotlib.pyplot as plit
