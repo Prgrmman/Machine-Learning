779dc80e80a87f2c0d964fb8c797e8d1229f0cf5
Jon Terner <jon.programming@gmail.com>
2017-04-18 09:13:45 -0500

    separate scripts

--- analysis/feature-test.py
+++ analysis/feature-test.py
@@ -22,11 +22,13 @@ absences = absences.astype(numpy.float)
 g3_score = g3_score.reshape(-1,1)
 absences = absences.reshape(-1,1)
 
-#divide into train and test sets randomly
-g3_score_train, g3_score_test, absences_train, absences_test = cross_validation.train_test_split(g3_score, absences, test_size=0.2)
 
-#use linear regression model
-model = LinearRegression()
-model.fit(g3_score_train,absences_train)
+
+# prints summary of of regression
+X = sm.add_constant(absences)
+olsModel = sm.OLS(g3_score, X)
+est = olsModel.fit()
+print(est.summary())
+
 
 
