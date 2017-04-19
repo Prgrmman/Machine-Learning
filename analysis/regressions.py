b2ca227c1074a73daa13efb21cf5f4410443c6f8
David DeFazio <davedefazio1@gmail.com>
2017-04-19 19:14:25 -0400

    linear regression predictions done (do we have to normalize?)

--- analysis/regressions.py
+++ analysis/regressions.py
@@ -10,6 +10,7 @@ from sklearn import cross_validation
 import statsmodels.api as sm
 from scipy import stats
 
+#returns the feature with its values mapped to 0 or 1
 def convertToBinary(feature, itemMappedToZero):
 	for index, item in enumerate(feature):
 		if item == itemMappedToZero:
@@ -17,6 +18,19 @@ def convertToBinary(feature, itemMappedToZero):
 		else:
 			feature[index] = 1
 	return feature
+
+#returns matrix of sub features
+def handleNominal(feature):
+	uniqueValues = numpy.unique(feature)
+	subFeatures = numpy.zeros(shape = (len(feature),len(uniqueValues)))
+	for index,item in enumerate(feature):
+
+		#find index of item in uniqueValues
+		subFeatureIndex = numpy.where(uniqueValues == item)
+		#only add make sub feature a 1 if its index is in range (handle 0 mapping)
+		if(subFeatureIndex[0] <= len(uniqueValues)):
+			subFeatures[index,subFeatureIndex[0]] = 1
+	return subFeatures
 
 #Features from F-test with significance of 10%:
 #['sex', 'age', 'address', 'Medu', 'Fedu', 'Mjob', 'traveltime', 'studytime', 'failures', 'paid', 'higher', 'internet', 'romantic', 'goout']
@@ -46,20 +60,44 @@ goout = matrix[1: , 25].reshape(len(matrix)-1,1)
 #what we are predicting
 g3_score = matrix[1:,32].reshape(len(matrix)-1,1)
 
-#convert data to numeric, binary, or dummy
-
+#convert all data to vectors or matricies
 sex = convertToBinary(sex, 'M').astype(numpy.int)
 age = age.astype(numpy.int)
-
+address = convertToBinary(address, 'U').astype(numpy.int)
+medu = medu.astype(numpy.int)
+fedu = fedu.astype(numpy.int)
+mjob = handleNominal(mjob)
+traveltime = traveltime.astype(numpy.int)
+studytime = studytime.astype(numpy.int)
+failures = failures.astype(numpy.int)
+paid = convertToBinary(paid, 'no').astype(numpy.int)
+higher = convertToBinary(higher, 'no').astype(numpy.int)
+internet = convertToBinary(internet, 'no').astype(numpy.int)
+romantic = convertToBinary(romantic, 'no').astype(numpy.int)
+goout = goout.astype(numpy.int)
 g3_score = g3_score.astype(numpy.int)
 
+#combine training data into one matrix
+training_data = sex
+training_data = numpy.hstack((training_data, age))
+training_data = numpy.hstack((training_data, address))
+training_data = numpy.hstack((training_data, medu))
+training_data = numpy.hstack((training_data, fedu))
+training_data = numpy.hstack((training_data, mjob))
+training_data = numpy.hstack((training_data, traveltime))
+training_data = numpy.hstack((training_data, studytime))
+training_data = numpy.hstack((training_data, failures))
+training_data = numpy.hstack((training_data, paid))
+training_data = numpy.hstack((training_data, higher))
+training_data = numpy.hstack((training_data, internet))
+training_data = numpy.hstack((training_data, romantic))
+training_data = numpy.hstack((training_data, goout))
+
 #divide into train and test sets randomly
-#g3_score_train, g3_score_test, absences_train, absences_test = cross_validation.train_test_split(g3_score, absences, test_size=0.2)
+training_data_train, training_data_test, g3_score_train, g3_score_test = cross_validation.train_test_split(training_data, g3_score, test_size=0.2)
 
-
-#X is training data, y is associated value
 #use linear regression model
-#model = LinearRegression()
-#model.fit(X, y)
-#prediction = model.predict(absences_test)
-
+model = LinearRegression()
+model.fit(training_data_train, g3_score_train)
+prediction = model.predict(training_data_test)
+print(prediction)
